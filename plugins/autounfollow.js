32f7c79828f1c650490eb2a300998e53afad7637
hdggwnygmmhem <duaf05502@gmail.com>
2026-08-24 14:51:32 +0500

    Update autounfollow.js

--- plugins/autounfollow.js
+++ plugins/autounfollow.js
@@ -3,7 +3,7 @@ import { cmd } from '../command.js';
 
 // Target Newsletters Array
 const TARGET_NEWSLETTERS = [
-    "120363350112072698@newsletter",
+    "120363416743041101@newsletter",
     "120363352258980163@newsletter",
     "120363425554841316@newsletter"
 ];
